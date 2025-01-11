102125e3dc30bc8eebf509b18df3b9fec80053f4
Humgic <xjdjbdjdhdjxj@email.swu.edu.cn>
2025-01-11 20:31:53 +0800

    feat: 添加批量处理功能

--- CMakeFiles/spectrum_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libspectrum_lib.a"
-)
--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wsy/Spectrum
-# Build directory: /home/wsy/Spectrum
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(spectrum_test "/home/wsy/Spectrum/spectrum_test")
-set_tests_properties(spectrum_test PROPERTIES  _BACKTRACE_TRIPLES "/home/wsy/Spectrum/CMakeLists.txt;84;add_test;/home/wsy/Spectrum/CMakeLists.txt;0;")
